1135a68a3651399fcde56fbe4ce177e1e1ecfc0c
UOC\JGRECHM1 <JGRECHM1@stu.chi.ac.uk>
2024-01-15 13:45:02 +0000

    Fighter Jet moving/ No controls

---
+++ Assets/Scripts/JetControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetControls : MonoBehaviour
+{
+    //thrust moves jet forward
+    public float thrust;
+
+    //multiplies thrust value
+    public float thrust_multiplier;
+
+    //multiplies yaw value around the Y axis
+    public float yaw_mulitplier;
+
+    //multiplies yaw value around X axis
+    public float pitch_multiplier;
+
+    new Rigidbody rigidbody;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float pitch = Input.GetAxis("Vertical");
+        float yaw = Input.GetAxis("Horizontal");
+
+        rigidbody.AddRelativeForce(0f, 0f, thrust * thrust_multiplier * Time.deltaTime);
+    }
+
+
+}
