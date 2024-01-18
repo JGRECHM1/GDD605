db46d966f97c5cd473003ec21a49cd5f4ecc9e8c
JGRECHM1 <jamesmarguerat@gmail.com>
2024-01-18 21:50:47 +0000

    countdown finished

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CountdownTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float timeValue = 120;
+    public TextMeshProUGUI timeText;
+
+
+
     void Update()
     {
-        
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        float milliseconds = timeToDisplay % 1 * 1000;
+
+        timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
     }
 }
