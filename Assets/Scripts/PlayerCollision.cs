9b09710a89e7adbf0ad3898226396f75ef72a067
JGRECHM1 <jamesmarguerat@gmail.com>
2024-01-19 00:24:35 +0000

    made a booboo

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerCollision : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Terrain"))
+        {
+            PlayerManager.isGameOver = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+
+
+
+
+}
